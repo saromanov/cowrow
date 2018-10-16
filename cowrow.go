c700bec0f9e965f6c1f064cefccf002be24b24e2
Sergey <xxsmotur@gmail.com>
2018-10-16 23:49:24 +0500

    Moving envPath as attribute

--- cowrow.go
+++ cowrow.go
@@ -8,11 +8,9 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-const envPath = "COWROW"
-
 // LoadYAMLFile provides loading of the yaml file
 // by the name from path
-func LoadYAMLFile(name string, cfg interface{}) error {
+func LoadYAMLFile(envPath, name string, cfg interface{}) error {
 	path := os.Getenv(envPath)
 	fileConfig, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", path, name))
 	if err != nil {
