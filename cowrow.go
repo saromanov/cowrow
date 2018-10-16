57a5c033b6703033857e99e86c464e24cab83303
Sergey <xxsmotur@gmail.com>
2018-10-16 23:50:26 +0500

    Define error for not found paths

--- cowrow.go
+++ cowrow.go
@@ -1,6 +1,7 @@
 package cowrow
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,10 +9,15 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var errPathNotFound = errors.New("path is not found")
+
 // LoadYAMLFile provides loading of the yaml file
 // by the name from path
 func LoadYAMLFile(envPath, name string, cfg interface{}) error {
 	path := os.Getenv(envPath)
+	if path == "" {
+		return errPathNotFound
+	}
 	fileConfig, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", path, name))
 	if err != nil {
 		return err
